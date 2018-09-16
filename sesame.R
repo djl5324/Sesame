605903cbfad5f50abdeb563775533b617289a8a8
Daniel Laplante <42614953+djl5324@users.noreply.github.com>
2018-09-16 15:47:37 -0400

    Update sesame.R

--- sesame.R
+++ sesame.R
@@ -6,22 +6,162 @@ sesame$diffbody = sesame$postbody - sesame$prebody
 sesame$difflet = sesame$postlet - sesame$prelet
 sesame$diffnumb = sesame$postnumb - sesame$prenumb
 
+#Advantaged vs. disadvantaged
+sesamedisadv = sesame[sesame$site == 1 | sesame$site == 4 | sesame$site == 5,]
+sesameadv = sesame[sesame$site == 2 | sesame$site == 3,]
+
+#Exploratory Data Analysis
 par(mfrow = c(1, 3))
-#Comparing viewcat = 1 (rarely watch) to viewcat = 2, 3, 4
-body=lm(diffbody ~ factor(viewcat), data = sesame)
-summary(body)
+
 boxplot(sesame$diffbody ~ sesame$viewcat, horizontal = FALSE, main = "Overall Change in Body Scores",
         xlab = "Viewing Categories", ylab = "Body Pretest - Body Posttest", ylim = c(-25, 25))
 
-let=lm(difflet ~ factor(viewcat), data = sesame)
-summary(let)
 boxplot(sesame$difflet ~ sesame$viewcat, horizontal = FALSE, main = "Overall Change in Letters Scores",
         xlab = "Viewing Categories", ylab = "Letters Pretest - Letters Posttest", ylim = c(-45,45))
 
-numb=lm(diffnumb ~ factor(viewcat), data = sesame)
-summary(numb)
 boxplot(sesame$diffnumb ~ sesame$viewcat, horizontal = FALSE, main = "Overall Change in Numbers Scores",
         xlab = "Viewing Categories", ylab = "Numbers Pretest - Numbers Posttest", ylim = c(-45,45))
+
+par(mfrow = c(3, 2))
+
+boxplot(sesamedisadv$diffbody ~ sesamedisadv$viewcat, horizontal = FALSE,
+        main = "Change in Body Scores for Disadvantaged Children", xlab = "Viewing Categories",
+        ylab = "Body Pretest - Body Posttest", ylim = c(-25, 25))
+
+boxplot(sesameadv$diffbody ~ sesameadv$viewcat, horizontal = FALSE,
+        main = "Change in Body Scores for Advantaged Children", xlab = "Viewing Categories",
+        ylab = "Body Pretest - Body Posttest", ylim = c(-25, 25))
+
+boxplot(sesamedisadv$difflet ~ sesamedisadv$viewcat, horizontal = FALSE,
+        main = "Change in Letters Scores for Disadvantaged Children", xlab = "Viewing Categories",
+        ylab = "Letters Pretest - Letters Posttest", ylim = c(-45, 45))
+
+boxplot(sesameadv$difflet ~ sesameadv$viewcat, horizontal = FALSE,
+        main = "Change in Letters Scores for Advantaged Children", xlab = "Viewing Categories",
+        ylab = "Letters Pretest - Letters Posttest", ylim = c(-45, 45))
+
+boxplot(sesamedisadv$diffnumb ~ sesamedisadv$viewcat, horizontal = FALSE,
+        main = "Change in Numbers Scores for Disadvantaged Children", xlab = "Viewing Categories",
+        ylab = "Numbers Pretest - Numbers Posttest", ylim = c(-45, 45))
+
+boxplot(sesameadv$diffnumb ~ sesameadv$viewcat, horizontal = FALSE,
+        main = "Change in Numbers Scores for Advantaged Children", xlab = "Viewing Categories",
+        ylab = "Numbers Pretest - Numbers Posttest", ylim = c(-45, 45))
+
+#Regression
+par(mfrow = c(2, 2))
+
+body=lm(diffbody ~ factor(viewcat), data = sesame)
+summary(body)
+anova(body)
+plot(body)
+
+let=lm(difflet ~ factor(viewcat), data = sesame)
+summary(let)
+anova(let)
+plot(let)
+
+numb=lm(diffnumb ~ factor(viewcat), data = sesame)
+summary(numb)
+anova(numb)
+plot(numb)
+
+
+#adv vs. disadv
+bodydisadv=lm(diffbody ~ factor(viewcat), data = sesamedisadv)
+summary(bodydisadv)
+anova(bodydisadv)
+plot(bodydisadv, main = "bodydisadv")
+
+bodyadv=lm(diffbody ~ factor(viewcat), data = sesameadv)
+summary(bodyadv)
+anova(bodyadv)
+plot(bodyadv, main = "bodyadv")
+
+letdisadv=lm(difflet ~ factor(viewcat), data = sesamedisadv)
+summary(letdisadv)
+anova(letdisadv)
+plot(letdisadv, main = "letdisadv")
+
+letadv=lm(difflet ~ factor(viewcat), data = sesameadv)
+summary(letadv)
+anova(letadv)
+plot(letadv, main = "letadv")
+
+numbdisadv=lm(diffnumb ~ factor(viewcat), data = sesamedisadv)
+summary(numbdisadv)
+anova(numbdisadv)
+plot(numbdisadv, main = "numbdisadv")
+
+numbadv=lm(diffnumb ~ factor(viewcat), data = sesameadv)
+summary(numbadv)
+anova(numbadv)
+plot(numbadv, main = "numbadv")
+
+#Looking for confounders
+bodycon1=lm(diffbody ~ factor(viewcat) + factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
+bodycon2=lm(diffbody ~ factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
+anova(bodycon2, bodycon1)
+
+letcon1=lm(difflet ~ factor(viewcat) + factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
+letcon2=lm(difflet ~ factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
+anova(letcon2, letcon1)
+
+numbcon1=lm(diffnumb ~ factor(viewcat) + factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
+numbcon2=lm(diffnumb ~ factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
+anova(numbcon2, numbcon1)
+
+##EXTRA##
+
+#Comparing viewcat = 1 (rarely watch) to viewcat = 2, 3, 4
+
+#combine viewcat 2-4
+sesame$viewcatcombined = 1
+for (i in 1:240){
+  if (sesame$viewcat[i] == 1)
+    sesame$viewcatcombined[i] = 0
+}
+
+par(mfrow = c(2, 2))
+
+bodyc=lm(diffbody ~ factor(viewcatcombined), data = sesame)
+summary(bodyc)
+plot(bodyc)
+
+letc=lm(difflet ~ factor(viewcatcombined), data = sesame)
+summary(letc)
+plot(letc)
+
+numbc=lm(diffnumb ~ factor(viewcatcombined), data = sesame)
+summary(numbc)
+plot(numbc)
+
+bodydisadvc=lm(diffbody ~ factor(viewcatcombined), data = sesamedisadv)
+summary(bodydisadvc)
+plot(bodydisadvc)
+
+bodyadvc=lm(diffbody ~ factor(viewcatcombined), data = sesameadv)
+summary(bodyadvc)
+plot(bodyadvc)
+
+letdisadvc=lm(difflet ~ factor(viewcatcombined), data = sesamedisadv)
+summary(letdisadvc)
+plot(letdisadvc)
+
+letadvc=lm(difflet ~ factor(viewcatcombined), data = sesameadv)
+summary(letadvc)
+plot(letadvc)
+
+numbdisadvc=lm(diffnumb ~ factor(viewcatcombined), data = sesamedisadv)
+summary(numbdisadvc)
+plot(numbdisadvc)
+
+numbadvc=lm(diffnumb ~ factor(viewcatcombined), data = sesameadv)
+summary(numbadvc)
+plot(numbadvc)
+
+
+#By site
 
 #Splitting data by site
 sesame1 = sesame[sesame$site == 1,]
@@ -30,7 +170,6 @@ sesame3 = sesame[sesame$site == 3,]
 sesame4 = sesame[sesame$site == 4,]
 sesame5 = sesame[sesame$site == 5,]
 
-#By site
 
 body1=lm(diffbody ~ factor(viewcat), data = sesame1)
 summary(body1)
@@ -96,31 +235,19 @@ numb5=lm(diffnumb ~ factor(viewcat), data = sesame5)
 summary(numb5)
 boxplot(sesame5$diffnumb ~ sesame5$viewcat)
 
-#combine viewcat 2-4
-sesame$viewcatcombined = 1
-for (i in 1:240){
-  if (sesame$viewcat[i] == 1)
-    sesame$viewcatcombined[i] = 0
-}
 
-bodyc=lm(diffbody ~ factor(viewcatcombined), data = sesame)
-summary(bodyc)
+
+
+
+
+
 boxplot(sesame$diffbody ~ sesame$viewcatcombined)
 
-letc=lm(difflet ~ factor(viewcatcombined), data = sesame)
-summary(letc)
 boxplot(sesame$difflet ~ sesame$viewcatcombined)
 
-numbc=lm(diffnumb ~ factor(viewcatcombined), data = sesame)
-summary(numbc)
 boxplot(sesame$diffnumb ~ sesame$viewcatcombined)
 
-#Splitting data by site
-sesame1 = sesame[sesame$site == 1,]
-sesame2 = sesame[sesame$site == 2,]
-sesame3 = sesame[sesame$site == 3,]
-sesame4 = sesame[sesame$site == 4,]
-sesame5 = sesame[sesame$site == 5,]
+
 
 #By site
 
@@ -188,78 +315,14 @@ numb5c=lm(diffnumb ~ factor(viewcatcombined), data = sesame5)
 summary(numb5c)
 boxplot(sesame5$diffnumb ~ sesame5$viewcatcombined)
 
-#Looking for confounders
-bodycon=lm(diffbody ~ factor(viewcatcombined) + factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
-summary(bodycon)
-
-letcon=lm(difflet ~ factor(viewcatcombined) + factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
-summary(letcon)
-
-numbcon=lm(diffnumb ~ factor(viewcatcombined) + factor(viewenc) + sex + age + factor(setting) + peabody, data = sesame)
-summary(numbcon)
-
-#Advantaged vs. disadvantaged
-sesamedisadv = sesame[sesame$site == 1 | sesame$site == 4 | sesame$site == 5,]
-sesameadv = sesame[sesame$site == 2 | sesame$site == 3,]
-
-par(mfrow = c(3, 2))
-
-bodydisadv=lm(diffbody ~ factor(viewcat), data = sesamedisadv)
-summary(bodydisadv)
-boxplot(sesamedisadv$diffbody ~ sesamedisadv$viewcat, horizontal = FALSE,
-        main = "Change in Body Scores for Disadvantaged Children", xlab = "Viewing Categories",
-        ylab = "Body Pretest - Body Posttest", ylim = c(-25, 25))
-
-bodyadv=lm(diffbody ~ factor(viewcat), data = sesameadv)
-summary(bodyadv)
-boxplot(sesameadv$diffbody ~ sesameadv$viewcat, horizontal = FALSE,
-        main = "Change in Body Scores for Advantaged Children", xlab = "Viewing Categories",
-        ylab = "Body Pretest - Body Posttest", ylim = c(-25, 25))
-
-letdisadv=lm(difflet ~ factor(viewcat), data = sesamedisadv)
-summary(letdisadv)
-boxplot(sesamedisadv$difflet ~ sesamedisadv$viewcat, horizontal = FALSE,
-        main = "Change in Letters Scores for Disadvantaged Children", xlab = "Viewing Categories",
-        ylab = "Letters Pretest - Letters Posttest", ylim = c(-45, 45))
-
-letadv=lm(difflet ~ factor(viewcat), data = sesameadv)
-summary(letadv)
-boxplot(sesameadv$difflet ~ sesameadv$viewcat, horizontal = FALSE,
-        main = "Change in Letters Scores for Advantaged Children", xlab = "Viewing Categories",
-        ylab = "Letters Pretest - Letters Posttest", ylim = c(-45, 45))
-
-numbdisadv=lm(diffnumb ~ factor(viewcat), data = sesamedisadv)
-summary(numbdisadv)
-boxplot(sesamedisadv$diffnumb ~ sesamedisadv$viewcat, horizontal = FALSE,
-        main = "Change in Numbers Scores for Disadvantaged Children", xlab = "Viewing Categories",
-        ylab = "Numbers Pretest - Numbers Posttest", ylim = c(-45, 45))
-
-numbadv=lm(diffnumb ~ factor(viewcat), data = sesameadv)
-summary(numbadv)
-boxplot(sesameadv$diffnumb ~ sesameadv$viewcat, horizontal = FALSE,
-        main = "Change in Numbers Scores for Advantaged Children", xlab = "Viewing Categories",
-        ylab = "Numbers Pretest - Numbers Posttest", ylim = c(-45, 45))
-
-bodydisadvc=lm(diffbody ~ factor(viewcatcombined), data = sesamedisadv)
-summary(bodydisadvc)
 boxplot(sesamedisadv$diffbody ~ sesamedisadv$viewcatcombined)
 
-letdisadvc=lm(difflet ~ factor(viewcatcombined), data = sesamedisadv)
-summary(letdisadvc)
 boxplot(sesamedisadv$difflet ~ sesamedisadv$viewcatcombined)
 
-numbdisadvc=lm(diffnumb ~ factor(viewcatcombined), data = sesamedisadv)
-summary(numbdisadvc)
 boxplot(sesamedisadv$diffnumb ~ sesamedisadv$viewcatcombined)
 
-bodyadvc=lm(diffbody ~ factor(viewcatcombined), data = sesameadv)
-summary(bodyadvc)
 boxplot(sesameadv$diffbody ~ sesameadv$viewcatcombined)
 
-letadvc=lm(difflet ~ factor(viewcatcombined), data = sesameadv)
-summary(letadvc)
 boxplot(sesameadv$difflet ~ sesameadv$viewcatcombined)
 
-numbadvc=lm(diffnumb ~ factor(viewcatcombined), data = sesameadv)
-summary(numbadvc)
 boxplot(sesameadv$diffnumb ~ sesameadv$viewcatcombined)
